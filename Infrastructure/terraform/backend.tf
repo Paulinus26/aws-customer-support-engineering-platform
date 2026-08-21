ab4be06e3daf208aa06a7497209bcfb134d5502c
Paulinus Chukwurah <paulinuschukwurah@gmail.com>
2026-08-21 15:18:57 +0100

    ci(terraform): sync workflow and backend with S3 remote state

--- Infrastructure/terraform/backend.tf
+++ Infrastructure/terraform/backend.tf
@@ -1,15 +1,9 @@
-# Terraform Remote State
-##################################################
-
-# This backend will be enabled after the S3 state bucket
-# and DynamoDB lock table are created.
-
-# terraform {
-#   backend "s3" {
-#     bucket         = "supportdesk-terraform-state"
-#     key            = "dev/terraform.tfstate"
-#     region         = "us-east-1"
-#     encrypt        = true
-#     use_lockfile   = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket       = "supportdesk-terraform-state-963108102569"
+    key          = "supportdesk/dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
